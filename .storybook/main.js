8322d832ba9f0142aec5c493ecba9350d3c68eb5
alanhe <qianghe421@gmail.com>
2022-04-23 11:51:02 +0800

    add @stacker/storybook-webpack-auto-title-loader

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,19 @@
+const path = require('path');
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
-  ],
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-create-react-app"],
   "framework": "@storybook/react",
   "core": {
     "builder": "webpack5"
+  },
+  webpackFinal: async (config, {configType}) => {
+    config.module.rules.push({
+      test: /\.stories\.(jsx?$|tsx?$)/, use: [{
+        loader: '@stacker/storybook-webpack-auto-title-loader', options: {
+          appendTitle: ({title, componentName}) => `${title}-${componentName}`
+        }
+      }], include: path.resolve(__dirname, '../src'),
+    });
+    return config;
   }
 }
